1d7c1f247fea40f343bb7e810f7c6df7d305e643
Devendra Singh <devendra.tarento@uidai.com>
2019-06-21 18:11:09 +0530

    Added recipe new and edit form using ReactiveFormModule method

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,10 +2,11 @@ import { Recipe } from './recipe.model';
 import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
   
 @Injectable()
 export class RecipeService{
-    
+    recipeChanged = new Subject<Recipe[]>();
     private recipes: Recipe[] = [
         new Recipe(
                     "Test Recipe 1", 
@@ -40,6 +41,16 @@ export class RecipeService{
 
     addIngredientsToShoppinglist(ingredients: Ingredient[]){
         this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe){
+        this.recipes.push(recipe);
+        this.recipeChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index:number, newRecipe: Recipe){
+        this.recipes[index]= newRecipe; 
+        this.recipeChanged.next(this.recipes.slice())
     }
 
 }
